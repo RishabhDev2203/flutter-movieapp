575cc3ec27e75c27f308f0a3cbd0f9dfdc850048
Goutam Patil <goutam.patil@idealittechno.com>
2022-04-18 18:18:01 +0530

    Homepage design

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
 
   //Font Families
-  static const fontFamily1 = "PlayfairDisplay";
+  static const fontFamily = "SFProDisplay";
   static const fontFamily2 = "Inter";
 
   // OneSignal: for push
